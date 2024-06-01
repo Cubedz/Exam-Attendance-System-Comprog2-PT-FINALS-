e82e05a28cc3c3b41a633f2c1cad3f43c07c0562
Cubedz <34nitrocubed34@gmail.com>
2024-06-01 14:04:26 +0800

    UPDATE

--- ExamAttendanceChecker.java
+++ ExamAttendanceChecker.java
@@ -1,3 +1,27 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class ExamAttendanceChecker {
+   private static final String attendanceFile = "C:\\IZECUBES\\COLLEGE\\1ST YEAR\\SEM 2\\Comprog 2\\ACTIVITIES\\FINALS PT\\Exam-Attendance-System-Comprog2-PT-FINALS-//AttendanceTracker.txt";
+
    
+    public static void updateAttendance(String studentID, String studentName) throws IOException{
+        FileWriter writer = null;
+        try{
+           writer = new FileWriter(attendanceFile, true);
+           writer.write(String.format("%s,%s,%s\n",studentID, studentName,getCurrentDate()));
+
+        }catch (Exception e){
+            System.out.println(e);
+        }finally{
+            if (writer != null);
+            writer.close();
+        }
+    }
+
+    public static String getCurrentDate(){
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        return timeStamp;
+    }
 }
---
+++ ExamAttendanceSystem.java
@@ -0,0 +1,3 @@
+public class ExamAttendanceSystem {
+//MainCodes
+}
