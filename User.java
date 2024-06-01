1d55e109c206d680018c87beeaf093fdbb3fe55b
Negateeeive <wanderingelaina45@gmail.com>
2024-06-01 22:14:13 +0800

    created a file for ExamEligibility

--- User.java
+++ User.java
@@ -1,3 +1,26 @@
 public class User {
 //test
+
+    private String userId;
+    private String name;
+    private boolean isEligible;
+
+    public User(String userId, String name){
+        this.userId = userId;
+        this.name = name;
+        this.isEligible = false;
+    }
+    public String getUserId(){
+        return userId;
+    }
+    public String getName(){
+        return name;
+    }
+    public boolean isEligible(){
+        return isEligible;
+    }
+    public void setEligible(boolean isEligible){
+        this.isEligible = isEligible;
+    }
+
 }
