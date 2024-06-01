89c843994eac882d82b2dcbce2eefc42bf8d8227
Cubedz <34nitrocubed34@gmail.com>
2024-06-01 19:46:06 +0800

    examAT updt

--- ExamAttendanceChecker.java
+++ ExamAttendanceChecker.java
@@ -6,6 +6,7 @@ import java.util.*;
 public class ExamAttendanceChecker {
    private static final String attendanceFile = "C:\\IZECUBES\\COLLEGE\\1ST YEAR\\SEM 2\\Comprog 2\\ACTIVITIES\\FINALS PT\\Exam-Attendance-System-Comprog2-PT-FINALS-//AttendanceTracker.txt";
 
+  
     public static void updateAttendance(int studentID, String studentName) throws IOException{
         FileWriter writer = null;
         try{
@@ -21,7 +22,12 @@ public class ExamAttendanceChecker {
     }
     
     public static void displayIfAttended(String line){
-        
+            if (line.matches(".*ATTENDED$")){
+                System.out.println("The student Attended on " + getCurrentDate());
+            }
+            else {
+                System.out.println("No Attendance Recorded");
+            }
     }
 
     public static String getCurrentDate(){
