2404567aab5db0fbb3da6777bd0204aa96527e4b
Seandarating <haruotsuumi@gmail.com>
2024-06-02 20:28:02 +0800

    dinagdagan ko ng curly brace, saka inadjust ung iba kasi nalilito ako

--- StudentLogin.java
+++ StudentLogin.java
@@ -2,21 +2,27 @@
 
 import java.util.Scanner;
 import java.io.*;
-public class StudentLogin extends User{
+public class StudentLogin extends User
+{
     private static final Scanner sc = new Scanner(System.in);
 
-    static void StudentLogin() throws InterruptedException{
+    static void StudentLogin() throws InterruptedException
+    {
             System.out.println("Please enter your username and password");
                 System.out.println("Username: ");
                 String username = sc.nextLine();
                 System.out.println("Password: ");
                 String password = sc.nextLine();
 
-        if(validateInput.checkUnamePword(username, password)){
+        if(validateInput.checkUnamePword(username, password))
+        {
             System.out.println("Log in Successful");
             Shortcuts.clearScreen();
     
-        }else{
+        }
+        else
+        {
             System.out.println("Invalid Username OR Password.");
         }
+    }
 }
