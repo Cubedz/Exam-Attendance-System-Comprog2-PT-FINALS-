93e461eace9a97d8669fe77bf96116415175e36f
jabakarlos <josetacbad45@gmail.com>
2024-06-02 20:08:19 +0800

    Added recycle code of the last pt about login

--- Shortcuts.java
+++ Shortcuts.java
@@ -1,3 +1,23 @@
-public class Shortcuts {
+//try lang ni-recycle ko lang yung sa pt natin dati
 
+public class Shortcuts {
+    static void clearScreen() throws InterruptedException{
+        Thread.sleep(2000);
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    static boolean isAlphaNumeric(String in){
+        String alphaChecker = "[a-zA-Z0-9]+( [a-zA-Z0-9]+)?";
+        return in.matches(alphaChecker);
+    }
+
+    static boolean isAlphaNumericPassword(String input){
+        String alphaChecker = "[a-zA-Z0-9]+";
+        return input.matches(alphaChecker);
+    }
+
+    static boolean empty(String input) {
+        return input.trim().isEmpty();
+    }
 }
