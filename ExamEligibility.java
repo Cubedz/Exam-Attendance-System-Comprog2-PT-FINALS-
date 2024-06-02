948fff4fd799f97321187c4571e5a8f55868d277
Negateeeive <wanderingelaina45@gmail.com>
2024-06-02 19:13:24 +0800

    deleted payment updater file and other contents in exam eligibility and user java

--- ExamEligibility.java
+++ ExamEligibility.java
@@ -1,15 +1,4 @@
 public class ExamEligibility {
 
-        public static void main(String[] args) {
-            User user = new User());
-            PaymentUpdater pu = new PaymentUpdater();
-    
-            pu.UpdateandcheckPayStatus(user);
-    
-            if (user.isEligible()) {
-                System.out.println("User " + user.getName() + " (" + user.getUserId() + ") is eligible to take the exam.");
-            } else {
-                System.out.println("User " + user.getName() + " (" + user.getUserId() + ") is not eligible to take the exam. Payment is required.");
-            }
-        }
+       
     }
