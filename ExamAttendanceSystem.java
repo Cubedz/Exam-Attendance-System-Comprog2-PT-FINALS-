b2767ea38b6a25e69252f9cbb459b81235777cb2
Cubedz <34nitrocubed34@gmail.com>
2024-06-03 23:29:06 +0800

    FLOW??

--- ExamAttendanceSystem.java
+++ ExamAttendanceSystem.java
@@ -1,3 +1,10 @@
 public class ExamAttendanceSystem {
-//MainCodes
+
+/*Admin Log-in
+ * Once logged in, get student info
+ * display if student is eligible for exam
+ * if eligible, record their attendance
+ * continue the program once all students have been recorded
+ */
+
 }
