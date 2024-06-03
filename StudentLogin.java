9d338108d13600667391b084f8813e53659c3e0d
Seandarating <haruotsuumi@gmail.com>
2024-06-04 00:48:35 +0800

    pacheck...

--- StudentLogin.java
+++ StudentLogin.java
@@ -1,29 +1,71 @@
 //try lang ni-recycle ko lang yung sa pt natin dati
-/* 
-import java.util.Scanner;
+
+import java.util.*;
 import java.io.*;
 public class StudentLogin extends User
 {
+    private static final String loginFile = "C:\\Users\\Ruika\\Desktop\\Folder ni Andrei XD\\STI - CS101A\\Computer Programming\\CODESSS\\VSCxGit\\Exam-Attendance-System-Comprog2-PT-FINALS-\\Exam-Attendance-System-Comprog2-PT-FINALS-";
     private static final Scanner sc = new Scanner(System.in);
 
-    static void StudentLogin() throws InterruptedException
+    static void StudentLogin() throws IOException, InterruptedException
     {
-            System.out.println("Please enter your username and password");
-                System.out.println("Username: ");
-                String username = sc.nextLine();
-                System.out.println("Password: ");
-                String password = sc.nextLine();
+        boolean loggedin = false;
 
-        if(validateInput.checkUnamePword(username, password))
+        while(!loggedin)
         {
-            System.out.println("Log in Successful");
-            Shortcuts.clearScreen();
-    
-        }
-        else
-        {
-            System.out.println("Invalid Username OR Password.");
+            System.out.println("Please Enter Your Full Name and Student ID: ");
+            System.out.println("Full Name: ");
+            String name = sc.nextLine();
+            System.out.println("Student ID (02000xxxxxx): ");
+            String sID = sc.nextLine();
+        
+
+            if(Shortcuts.isAlphaOnly(name) && Shortcuts.isNumericID(sID) && !Shortcuts.empty(name) && !Shortcuts.empty(sID))
+            {
+                if (checkNameID(name, sID))
+                {
+                    System.out.println("Logged in Successfully.");
+                    loggedin = true;
+                    Shortcuts.clearScreen();
+                }
+                else
+                {
+                    System.out.println("Name or ID cannot be found.");
+                    Shortcuts.clearScreen();
+                    continue;
+                }
+            }
+
+        
+            else
+            {
+                System.out.println("Please Refer to the Format.");
+            }
         }
     }
+
+    public static boolean checkNameID(String name, String ID)
+    {
+        try
+        {
+            BufferedReader br = new BufferedReader(new FileReader(loginFile));
+            String checkline;
+
+            while((checkline = br.readLine())!=null)
+            {
+                String fileNAME = checkline;
+                String fileID = br.readLine();
+
+                if(fileNAME.equals(name) && fileID.equals(ID))
+                {
+                    return true;
+                }
+            }
+        }
+        catch(IOException ioe)
+        {
+            System.out.println(ioe.getMessage());
+        }
+        return false;
+    }
 }
-*/
