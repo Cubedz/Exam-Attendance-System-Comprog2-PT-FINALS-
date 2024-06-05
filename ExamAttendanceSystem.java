82212926a6ce9ae035bbc5fe608fb79945bf5b8d
Cubedz <34nitrocubed34@gmail.com>
2024-06-05 16:02:53 +0800

    FLOW OF PROGRAM

--- ExamAttendanceSystem.java
+++ ExamAttendanceSystem.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class ExamAttendanceSystem {
 
 /*Admin Log-in
@@ -8,4 +9,32 @@ public class ExamAttendanceSystem {
  * continue the program once all students have been recorded
  */
 
+//Front End prototype
+ public static void main(String[] args) {
+    String a;
+    String b;
+    String c;
+    String d;
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Welcome Admin");
+    System.out.println("Enter you username: ");
+    a = sc.nextLine();
+    System.out.println("Enter your password");
+    b = sc.nextLine();
+
+    System.out.println("Succesfully loggged in");
+
+    System.out.println("Enter student id: ");
+    c = sc.nextLine();
+
+    System.out.println("Enter name: ");
+    d = sc.nextLine();
+
+    //method to check if paid or not
+    //IF student is paid, the program will mark the student as ATTENDED
+    //the program will repeat for the next student to enter their attendance
+    //the program will say if ALL students havce been recorded for that section
+
+ }
 }
