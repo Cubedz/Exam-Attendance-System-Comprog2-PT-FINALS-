c42ba5d5a4ab8bc70a3a27fb7070d3da2274155e
Cubedz <34nitrocubed34@gmail.com>
2024-06-05 22:58:11 +0800

    User FIXED Update

--- User.java
+++ User.java
@@ -1,66 +1,48 @@
-<<<<<<< HEAD
 public class User {
 
- private String studentId;
- private String name;
- private boolean isPaid;
 
- public User(String studentId, String name, boolean isPaid){
-    this.studentId = studentId;
-    this.name = name;
-    this.isPaid = isPaid;
- }
- public String getstudentId(){
-    return studentId;
- }
- public String getName(){
-    return name;
- }
- public boolean isPaid(){
-    return isPaid;
- }
- public boolean isEligible(){
-    return isPaid;
-  }
+   private String username;
+   private String password;
+   private String studentID;
+   private String studentName;
+
+   public String getUsername() {
+
+       return username;
+   }
+
+   public void setUsername(String username) {
+
+       this.username = username;
+   }
+
+   public String getPassword() {
+
+       return password;
+   }
+
+   public void setPassword(String password) {
   
-=======
-public class User 
-{
-   private String username, password, studentID, studentName;
-
-
-   public String getUsername()
-   {
-      return this.username = username;
-   }
-   public void setUsername(String username)
-   {
-      this.username = username;
-   }
-   public String getPassword()
-   {
-      return this.password = password;
-   }
-   public void setPassword(String password)
-   {
-      this.password = password;
-   }
-   public String getStudentID()
-   {
-      return studentID;
-   }
-   public void setStudentID(String studentID)
-   {
-      this.studentID = studentID;
-   }
-   public String getStudentName()
-   {
-      return studentName;
-   }
-   public void setStudentName(String studentName)
-   {
-      this.studentName = studentName;
+       this.password = password;
    }
 
->>>>>>> 59cb7bc9fb6c5916d2f8cac7edc7608203f4a8b6
+   public String getStudentID() {
+    
+       return studentID;
+   }
+
+   public void setStudentID(String student_id) {
+    
+       this.studentID = student_id;
+   }
+
+   public String getStudentName() {
+
+       return studentName;
+   }
+
+   public void setStudentName(String student_name) {
+
+       this.studentName = student_name;
+   }
 }
