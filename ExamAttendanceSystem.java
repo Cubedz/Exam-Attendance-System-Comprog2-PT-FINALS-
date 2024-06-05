e4fe81e84a2f20019a8a25beeb4d90fb70262464
Cubedz <34nitrocubed34@gmail.com>
2024-06-05 13:20:54 +0800

    asd

--- ExamAttendanceSystem.java
+++ ExamAttendanceSystem.java
@@ -4,6 +4,7 @@ public class ExamAttendanceSystem {
  * Once logged in, get student info
  * display if student is eligible for exam
  * if eligible, record their attendance
+ * then ask the next student for their ID and Name
  * continue the program once all students have been recorded
  */
 
